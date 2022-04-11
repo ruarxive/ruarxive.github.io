5ee926236cb9485fcc13e07a3bf80695d829ca2e
Mednikov <pavel.mednikov@infoculture.ru>
2022-04-11 03:30:44 +0300

    Deploy website - based on a0341b61c29bcc199f36a3961ea4ba1025495d65

--- en/js/payment.js
+++ en/js/payment.js
@@ -1,11 +1,46 @@
-this.pay = function () {
-  console.log("HEEEEEEEEEEEEEEEEEYYYYYYY");
+this.payMonthly = function () {
   var widget = new cp.CloudPayments();
   widget.pay('auth', // или 'charge'
       { //options
           publicId: 'pk_3018668db11eb861d16c3d8a73bf1', //id из личного кабинета
-          description: 'Оплата товаров в example.com', //назначение
+          description: 'Ежемесячная помощь проекту ruarxive.org', //назначение
           amount: 100, //сумма
+          currency: 'RUB', //валюта
+          requireEmail: true, //требование указать e-mail адрес пользователя в виджете
+          accountId: 'user@example.com', //идентификатор плательщика (необязательно)
+          // invoiceId: '1234567', //номер заказа  (необязательно)
+          // email: 'user@example.com', //email плательщика (необязательно)
+          skin: "mini", //дизайн виджета (необязательно)
+          data: {
+              CloudPayments: {
+                recurrent: {
+                  interval: 'Month',
+                  period: 1, 
+                }
+              }
+          }
+      },
+      {
+          onSuccess: function (options) { // success
+              //действие при успешной оплате
+          },
+          onFail: function (reason, options) { // fail
+              //действие при неуспешной оплате
+          },
+          onComplete: function (paymentResult, options) { //Вызывается как только виджет получает от api.cloudpayments ответ с результатом транзакции.
+              //например вызов вашей аналитики Facebook Pixel
+          }
+      }
+  )
+};
+
+this.paySingle = function () {
+  var widget = new cp.CloudPayments();
+  widget.pay('auth', // или 'charge'
+      { //options
+          publicId: 'pk_3018668db11eb861d16c3d8a73bf1', //id из личного кабинета
+          description: 'Помощь проекту ruarxive.org', //назначение
+          amount: 500, //сумма
           currency: 'RUB', //валюта
           // accountId: 'user@example.com', //идентификатор плательщика (необязательно)
           // invoiceId: '1234567', //номер заказа  (необязательно)
@@ -28,5 +63,5 @@ this.pay = function () {
       }
   )
 };
-console.log("HEEEEEEEEEEEEEEEEEYYYYYYY");
-$('#checkout').click(pay);
+
+// $('#checkout').click(pay);
